44f6ce3038c9c3d0fc793ff3ea7facdc74b84cca
nitesh <niteshrana754@gmail.com>
2020-07-21 17:21:24 +0530

    uncontrolled form onclick not working

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,6 @@ import { DISHES } from  '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
