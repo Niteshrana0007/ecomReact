343c29997eb05544fbb86bff324abf4c9448c236
Nitesh <niteshrana754@gmail.com>
2021-09-21 10:56:06 +0530

    Updates

---
+++ precache-manifest.4a7767a74f9be761e6ee7038bf35b899.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e2b91ec9b040d30e6fbdf7e576a7aa",
+    "url": "/ecomReact/index.html"
+  },
+  {
+    "revision": "2c1c8c6b290bbe26d062",
+    "url": "/ecomReact/static/css/2.35a0f4ae.chunk.css"
+  },
+  {
+    "revision": "f9abbdabdee4535dd6c2",
+    "url": "/ecomReact/static/css/main.6e62571d.chunk.css"
+  },
+  {
+    "revision": "2c1c8c6b290bbe26d062",
+    "url": "/ecomReact/static/js/2.7bbab06d.chunk.js"
+  },
+  {
+    "revision": "7980e0235556fcd1ae2b7b83ab42d77c",
+    "url": "/ecomReact/static/js/2.7bbab06d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9abbdabdee4535dd6c2",
+    "url": "/ecomReact/static/js/main.2004cadd.chunk.js"
+  },
+  {
+    "revision": "1863b02f0968a965f6d8",
+    "url": "/ecomReact/static/js/runtime-main.a2256695.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ecomReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ecomReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ecomReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ecomReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ecomReact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
